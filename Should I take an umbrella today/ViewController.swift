09f6a35d4aad95772a5abcf6dc99dbb851d70cd2
Khadyka Vadim <khadyka.vadim@gmail.com>
2019-10-12 13:51:30 +0300

    Initial Commit

---
+++ Should I take an umbrella today/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Should I take an umbrella today
+//
+//  Created by Vadim Khadyka on 10/12/19.
+//  Copyright © 2019 Vadim Khadyka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
